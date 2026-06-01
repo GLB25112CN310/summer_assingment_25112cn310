2a9f09c6bf2e710203d8c6449daaaa6ca39439f4
GLB25112CN310 <shauryachauhan2344@gmail.com>
2026-06-01 21:58:06 +0530

    Add files via upload

---
+++ day5_Q18.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    int n, fact, i, temp, rem, sum = 0;
+    printf("enter the number :");
+    scanf("%d",&n);
+    temp = n;
+    while(temp > 0){
+        rem = temp%10;
+        fact = 1;
+    
+        for(i=1;i<=rem;i++)
+            fact = fact*i;
+           
+        sum= sum+fact;
+        temp/=10;
+        
+    }    
+    if(sum == n){
+        printf("%d is a strong number",n);
+    }
+    else{
+        printf("%d is not a strong number ",n);
+    }
+}
