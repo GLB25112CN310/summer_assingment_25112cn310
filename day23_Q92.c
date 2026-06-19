f49ee13811b837cf4f5178ee30717dc234d9eab8
GLB25112CN310 <shauryachauhan2344@gmail.com>
2026-06-19 20:52:25 +0530

    Add files via upload

---
+++ day23_Q92.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int i, j, count, maxCount = 0;
+    char maxChar;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    for(i = 0; str[i] != '\0'; i++) {
+        count = 0;
+        for(j = 0; str[j] != '\0'; j++) {
+            if(str[i] == str[j])
+                count++;
+        }
+        if(count > maxCount) {
+            maxCount = count;
+            maxChar = str[i];
+        }
+    }
+    printf("Maximum occurring character: %c", maxChar);
+    printf("\nFrequency: %d", maxCount);
+    return 0;
+}
