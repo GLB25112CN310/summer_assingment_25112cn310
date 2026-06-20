5c78a51e0162e26bb6a8ffbb942d270b3a37c388
GLB25112CN310 <shauryachauhan2344@gmail.com>
2026-06-20 15:16:35 +0530

    Add files via upload

---
+++ day24_Q95.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[200], longest[100];
+    int i = 0, j = 0, maxLen = 0;
+    char word[100];
+    printf("Enter a sentence: ");
+    fgets(str, sizeof(str), stdin);
+    while(str[i] != '\0') {
+        if(str[i] != ' ' && str[i] != '\n') {
+            word[j++] = str[i];
+        } else {
+            word[j] = '\0';
+            if(j > maxLen) {
+                maxLen = j;
+                strcpy(longest, word);
+            }
+            j = 0;
+        }
+        i++;
+    }
+    printf("Longest word: %s", longest);
+    return 0;
+}
